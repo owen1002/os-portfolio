85e76f13d1ba65809b3a49905d59b500bcd562e7
Owen Siu <owensiu1002@gmail.com>
2024-04-27 16:58:50 +0800

    just a little css

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,7 +3,7 @@ import SocialMedia from "./SocialMedia";
 
 function Header() {
   return (
-    <div className="flex justify-between items-center border-b border-solid border-white pb-2 ">
+    <div className="flex justify-between items-center">
       <div className="text-6xl font-black ">Owen Siu</div>
       <div className="flex space-x-6 text-3xl">
         <SocialMedia />
